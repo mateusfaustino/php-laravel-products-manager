0513525d8797d3f3d7958034971adce9804f7cba
Mateus Faustino <mateusfaustino2017@gmail.com>
2021-09-17 10:42:11 -0300

    fix api route

--- src/routes/api.php
+++ src/routes/api.php
@@ -20,7 +20,6 @@ Route::post('/login', [AuthController::class, 'login']);
 Route::get('/products', [ProductController::class, 'index']);
 Route::get('/products/{id}', [ProductController::class, 'show']);
 Route::get('/products/search/{name}', [ProductController::class, 'search']);
-Route::post('/products', [ProductController::class, 'store']);
 
 Route::group(['middleware'=>['auth:sanctum']], function () {
     Route::post('/logout', [AuthController::class, 'logout']);
