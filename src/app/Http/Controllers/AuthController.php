7d91c8d36923986ccb025e8eb6d9d453de958f8e
Mateus Faustino <mateusfaustino2017@gmail.com>
2021-09-17 10:39:21 -0300

    create login

--- src/app/Http/Controllers/AuthController.php
+++ src/app/Http/Controllers/AuthController.php
@@ -15,18 +15,47 @@ class AuthController extends Controller
             'email'=>'required|string|unique:users,email',
             'password'=>'required|string|confirmed'
         ]);
-
+        
         $user = User::create([
             'name'=> $fields['name'],
             'email'=> $fields['email'],
             'password'=> bcrypt($fields['password']),
         ]);
-
+        
         $token = $user->createToken('myapptoken')->plainTextToken;
         $response = [
             'user'=>$user,
             'token'=> $token
         ];
         return response($response,201);
+    }
+    public function login(Request $request){
+        $fields = $request->validate([
+            'email'=>'required|string',
+            'password'=>'required|string'
+        ]);
+        
+        $user = User::where('email', $fields['email'])->first();
+
+        if(!$user || !Hash::check($fields['password'], $user->password)){
+            return response([
+                'message' => 'Bad creds'
+            ], 401);
+        }
+        
+        $token = $user->createToken('myapptoken')->plainTextToken;
+        $response = [
+            'user'=>$user,
+            'token'=> $token
+        ];
+        return response($response,201);
+    }
+
+    public function logout(Request $request) {
+        auth()->user()->tokens()->delete();
+
+        return [
+            'message' => 'Logged out'
+        ];
     }
 }
--- src/routes/api.php
+++ src/routes/api.php
@@ -16,12 +16,14 @@ use App\Http\Controllers\AuthController;
 |
 */
 Route::post('/register', [AuthController::class, 'register']);
+Route::post('/login', [AuthController::class, 'login']);
 Route::get('/products', [ProductController::class, 'index']);
 Route::get('/products/{id}', [ProductController::class, 'show']);
 Route::get('/products/search/{name}', [ProductController::class, 'search']);
 Route::post('/products', [ProductController::class, 'store']);
 
 Route::group(['middleware'=>['auth:sanctum']], function () {
+    Route::post('/logout', [AuthController::class, 'logout']);
     Route::post('/products', [ProductController::class, 'store']);
     Route::put('/products/{id}', [ProductController::class, 'update']);
     Route::delete('/products/{id}', [ProductController::class, 'destroy']);
